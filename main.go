0a7f08443dc5df8eca1efbaa3ad52c875ab51ed8
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-02-13 19:30:37 +0900

    commit

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
- 
+
 import (
 	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 
 	_ "github.com/mattn/go-sqlite3"
 )
- 
+
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*.html")
@@ -16,67 +17,66 @@ func main() {
 
 	//Index
 	router.GET("/", func(ctx *gin.Context) {
-		  todos := dbGetAll()
-		  ctx.HTML(200, "index.html", gin.H{
-				  "todos": todos,
-		  })
-  })
+		todos := dbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
+	})
 
-  //Create
-  router.POST("/new", func(ctx *gin.Context) {
-		  text := ctx.PostForm("text")
-		  status := ctx.PostForm("status")
-		  dbInsert(text, status)
-		  ctx.Redirect(302, "/")
-  })
+	//Create
+	router.POST("/new", func(ctx *gin.Context) {
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dbInsert(text, status)
+		ctx.Redirect(302, "/")
+	})
 
-  //Detail
-  router.GET("/detail/:id", func(ctx *gin.Context) {
-		  n := ctx.Param("id")
-		  id, err := strconv.Atoi(n)
-		  if err != nil {
-				  panic(err)
-		  }
-		  todo := dbGetOne(id)
-		  ctx.HTML(200, "detail.html", gin.H{"todo": todo})
-  })
+	//Detail
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+	})
 
 	//Update
 	router.POST("/update/:id", func(ctx *gin.Context) {
-			n := ctx.Param("id")
-			id, err := strconv.Atoi(n)
-			if err != nil {
-					panic("ERROR")
-			}
-			text := ctx.PostForm("text")
-			status := ctx.PostForm("status")
-			dbUpdate(id, text, status)
-			ctx.Redirect(302, "/")
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dbUpdate(id, text, status)
+		ctx.Redirect(302, "/")
 	})
 
 	//削除確認
 	router.GET("/delete_check/:id", func(ctx *gin.Context) {
-			n := ctx.Param("id")
-			id, err := strconv.Atoi(n)
-			if err != nil {
-					panic("ERROR")
-			}
-			todo := dbGetOne(id)
-			ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
 	})
 
 	//Delete
 	router.POST("/delete/:id", func(ctx *gin.Context) {
-			n := ctx.Param("id")
-			id, err := strconv.Atoi(n)
-			if err != nil {
-					panic("ERROR")
-			}
-			dbDelete(id)
-			ctx.Redirect(302, "/")
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dbDelete(id)
+		ctx.Redirect(302, "/")
 
 	})
-
 
 	router.Run()
 }
@@ -91,7 +91,7 @@ type Todo struct {
 func dbInit() {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
-			panic("データベース開けず！（dbInit）")
+		panic("データベース開けず！（dbInit）")
 	}
 	db.AutoMigrate(&Todo{})
 	defer db.Close()
@@ -101,7 +101,7 @@ func dbInit() {
 func dbInsert(text string, status string) {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
-			panic("データベース開けず！（dbInsert)")
+		panic("データベース開けず！（dbInsert)")
 	}
 	db.Create(&Todo{Text: text, Status: status})
 	defer db.Close()
@@ -111,7 +111,7 @@ func dbInsert(text string, status string) {
 func dbUpdate(id int, text string, status string) {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
-			panic("データベース開けず！（dbUpdate)")
+		panic("データベース開けず！（dbUpdate)")
 	}
 	var todo Todo
 	db.First(&todo, id)
@@ -125,7 +125,7 @@ func dbUpdate(id int, text string, status string) {
 func dbDelete(id int) {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
-			panic("データベース開けず！（dbDelete)")
+		panic("データベース開けず！（dbDelete)")
 	}
 	var todo Todo
 	db.First(&todo, id)
@@ -137,7 +137,7 @@ func dbDelete(id int) {
 func dbGetAll() []Todo {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
-			panic("データベース開けず！(dbGetAll())")
+		panic("データベース開けず！(dbGetAll())")
 	}
 	var todos []Todo
 	db.Order("created_at desc").Find(&todos)
@@ -149,7 +149,7 @@ func dbGetAll() []Todo {
 func dbGetOne(id int) Todo {
 	db, err := gorm.Open("sqlite3", "test.sqlite3")
 	if err != nil {
-			panic("データベース開けず！(dbGetOne())")
+		panic("データベース開けず！(dbGetOne())")
 	}
 	var todo Todo
 	db.First(&todo, id)
