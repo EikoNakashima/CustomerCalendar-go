678d2cac9b965ef5b9fe67a3e79e24b1c6ced5d4
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-02-17 21:12:18 +0900

    commit

--- main.go
+++ main.go
@@ -1,94 +1,18 @@
 package main
 
 import (
+	"gin_test/crypto"
+	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	_ "github.com/joho/godotenv/autoload"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("views/*.html")
-
-	dbInit()
-
-	//Index
-	router.GET("/", func(ctx *gin.Context) {
-		tweets := dbGetAll()
-		ctx.HTML(200, "index.html", gin.H{
-			"tweets": tweets,
-		})
-	})
-
-	//Create
-	router.POST("/new", func(ctx *gin.Context) {
-		var form Tweet
-		// ここがバリデーション部分
-		if err := ctx.Bind(&form); err != nil {
-			tweets := dbGetAll()
-			ctx.HTML(http.StatusBadRequest, "index.html", gin.H{"tweets": tweets, "err": err})
-			ctx.Abort()
-		} else {
-			content := ctx.PostForm("content")
-			status := ctx.PostForm("status")
-			dbInsert(content, status)
-			ctx.Redirect(302, "/")
-		}
-	})
-
-	//Detail
-	router.GET("/detail/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic(err)
-		}
-		tweet := dbGetOne(id)
-		ctx.HTML(200, "detail.html", gin.H{"tweet": tweet})
-	})
-
-	//Update
-	router.POST("/update/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		content := ctx.PostForm("content")
-		status := ctx.PostForm("status")
-		dbUpdate(id, content, status)
-		ctx.Redirect(302, "/")
-	})
-
-	//削除確認
-	router.GET("/delete_check/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		tweet := dbGetOne(id)
-		ctx.HTML(200, "delete.html", gin.H{"tweet": tweet})
-	})
-
-	//Delete
-	router.POST("/delete/:id", func(ctx *gin.Context) {
-		n := ctx.Param("id")
-		id, err := strconv.Atoi(n)
-		if err != nil {
-			panic("ERROR")
-		}
-		dbDelete(id)
-		ctx.Redirect(302, "/")
-
-	})
-
-	router.Run()
-}
 
 type Tweet struct {
 	gorm.Model
@@ -104,10 +28,10 @@ type User struct {
 }
 
 func gormConnect() *gorm.DB {
-	DBMS := "mysql"
-	USER := "test"
-	PASS := "12345678"
-	DBNAME := "test"
+	DBMS := os.Getenv("mytweet_DBMS")
+	USER := os.Getenv("mytweet_USER")
+	PASS := os.Getenv("mytweet_PASS")
+	DBNAME := os.Getenv("mytweet_DBNAME")
 	// MySQLだと文字コードの問題で"?parseTime=true"を末尾につける必要がある
 	CONNECT := USER + ":" + PASS + "@/" + DBNAME + "?parseTime=true"
 	db, err := gorm.Open(DBMS, CONNECT)
@@ -124,7 +48,76 @@ func dbInit() {
 
 	// コネクション解放解放
 	db.AutoMigrate(&Tweet{}) //構造体に基づいてテーブルを作成
+	db.AutoMigrate(&User{})
 	defer db.Close()
+}
+
+// ユーザー登録処理
+func createUser(username string, password string) []error {
+	db := gormConnect()
+	defer db.Close()
+	// Insert処理
+	if err := db.Create(&User{Username: username, Password: password}).GetErrors(); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+// ユーザーを一件取得
+func getUser(username string) User {
+	db := gormConnect()
+	var user User
+	db.First(&user, "username = ?", username)
+	db.Close()
+	return user
+}
+
+// つぶやき登録処理
+func createTweet(content string) {
+	db := gormConnect()
+	defer db.Close()
+	// Insert処理
+	db.Create(&Tweet{Content: content})
+}
+
+// つぶやき更新
+func updateTweet(id int, tweetText string) {
+	db := gormConnect()
+	var tweet Tweet
+	db.First(&tweet, id)
+	tweet.Content = tweetText
+	db.Save(&tweet)
+	db.Close()
+}
+
+// つぶやき全件取得
+func getAllTweets() []Tweet {
+	db := gormConnect()
+
+	defer db.Close()
+	var tweets []Tweet
+	// FindでDB名を指定して取得した後、orderで登録順に並び替え
+	db.Order("created_at desc").Find(&tweets)
+	return tweets
+}
+
+// つぶやき一件取得
+func getTweet(id int) Tweet {
+	db := gormConnect()
+	var tweet Tweet
+	db.First(&tweet, id)
+	db.Close()
+	return tweet
+}
+
+// つぶやき削除
+func deleteTweet(id int) {
+	db := gormConnect()
+	var tweet Tweet
+	db.First(&tweet, id)
+	db.Delete(&tweet)
+	db.Close()
 }
 
 // データインサート処理
@@ -174,4 +167,134 @@ func dbDelete(id int) {
 	db.First(&tweet, id)
 	db.Delete(&tweet)
 	db.Close()
+}
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*.html")
+
+	dbInit()
+
+	//Index
+	router.GET("/", func(ctx *gin.Context) {
+		tweets := dbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"tweets": tweets,
+		})
+	})
+
+	// ユーザー登録画面
+	router.GET("/signup", func(c *gin.Context) {
+
+		c.HTML(200, "signup.html", gin.H{})
+	})
+
+	// ユーザー登録
+	router.POST("/signup", func(c *gin.Context) {
+		var form User
+		// バリデーション処理
+		if err := c.Bind(&form); err != nil {
+			c.HTML(http.StatusBadRequest, "signup.html", gin.H{"err": err})
+			c.Abort()
+		} else {
+			username := c.PostForm("username")
+			password := c.PostForm("password")
+			// 登録ユーザーが重複していた場合にはじく処理
+			if err := createUser(username, password); err != nil {
+				c.HTML(http.StatusBadRequest, "signup.html", gin.H{"err": err})
+			}
+			c.Redirect(302, "/")
+		}
+	})
+
+	// ユーザーログイン画面
+	router.GET("/login", func(c *gin.Context) {
+
+		c.HTML(200, "login.html", gin.H{})
+	})
+
+	// ユーザーログイン
+	router.POST("/login", func(c *gin.Context) {
+
+		// DBから取得したユーザーパスワード(Hash)
+		dbPassword := getUser(c.PostForm("username")).Password
+		log.Println(dbPassword)
+		// フォームから取得したユーザーパスワード
+		formPassword := c.PostForm("password")
+
+		// ユーザーパスワードの比較
+		if err := crypto.CompareHashAndPassword(dbPassword, formPassword); err != nil {
+			log.Println("ログインできませんでした")
+			c.HTML(http.StatusBadRequest, "login.html", gin.H{"err": err})
+			c.Abort()
+		} else {
+			log.Println("ログインできました")
+			c.Redirect(302, "/")
+		}
+	})
+
+	// つぶやき登録
+	router.POST("/new", func(ctx *gin.Context) {
+		var form Tweet
+		// ここがバリデーション部分
+		if err := ctx.Bind(&form); err != nil {
+			tweets := dbGetAll()
+			ctx.HTML(http.StatusBadRequest, "index.html", gin.H{"tweets": tweets, "err": err})
+			ctx.Abort()
+		} else {
+			content := ctx.PostForm("content")
+			status := ctx.PostForm("status")
+			dbInsert(content, status)
+			ctx.Redirect(302, "/")
+		}
+	})
+
+	//つぶやきDetail
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		tweet := dbGetOne(id)
+		ctx.HTML(200, "detail.html", gin.H{"tweet": tweet})
+	})
+
+	//Update
+	router.POST("/update/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		tweet := ctx.PostForm("tweet")
+		status := ctx.PostForm("status")
+		dbUpdate(id, tweet, status)
+		ctx.Redirect(302, "/")
+	})
+
+	//削除確認
+	router.GET("/delete_check/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		tweet := dbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"tweet": tweet})
+	})
+
+	//Delete
+	router.POST("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dbDelete(id)
+		ctx.Redirect(302, "/")
+
+	})
+
+	router.Run()
 }
